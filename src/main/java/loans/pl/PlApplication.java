a7d99cbc817f68a5ba4207aa9f6ec6d399d5af75
venkat <venkat@tcs.com>
2025-04-11 07:16:57 +0530

    first project

---
+++ src/main/java/loans/pl/PlApplication.java
@@ -0,0 +1,13 @@
+package loans.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlApplication.class, args);
+	}
+
+}
